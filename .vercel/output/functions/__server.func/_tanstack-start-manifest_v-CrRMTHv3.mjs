e5ea0b556d90aea9a3a988176b2071ca99da3283
shankara_run <shankararunofficial@gmail.com>
2026-05-16 18:53:14 +0530

    chore: ignore .vercel build output

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CrRMTHv3.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/shankara_run/Projects/Code_Projects/website_showcase/visionary-web-solutions/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-CFS68h5K.js"] }, "/": { filePath: "/Users/shankara_run/Projects/Code_Projects/website_showcase/visionary-web-solutions/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Cjwcizz0.js"] } }, clientEntry: "/assets/index-CFS68h5K.js" });
-export {
-  tsrStartManifest
-};
